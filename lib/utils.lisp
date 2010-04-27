7309c2c76d39ef2361215c4f42f4e24a3467a29c
David Thole <dthole@x02c3.local>
2010-04-26 21:57:20 -0500

    Updated getClassVariables

--- lib/utils.lisp
+++ lib/utils.lisp
@@ -9,5 +9,8 @@
 
 (defun getClassVariables (&key (index 0) (file *data-set-file*))
   "Given an a default index 0 (first column), we parse the data set file to determine all the possible value options for our class."
-  
-  )
+  (with-data-file (s file)
+    (loop for line = (read-csv-line s) while line collecting (elt line index) into class-vars finally (return (delete-duplicates class-vars :test #'string-equal)))))
+
+(defun getAttributeVariables (&key (class-index 0) (file *data-set-file*))
+  "Given a class index, we'll parse all the other indexes, but that one, removing all the duplicates in the end.")
