6bdf8919d27198d782a116e12c263008c41e8b83
David Thole <dthole@x02c3.local>
2010-04-28 20:29:17 -0500

    Adding a getAllVariables function to pull together everything from the CSV file

--- lib/utils.lisp
+++ lib/utils.lisp
@@ -7,6 +7,16 @@
   `(with-open-file (,s ,file :direction :input :if-does-not-exist :error)
      ,@body))
 
+(defun getAllVariables (&key (file *data-set-file*))
+  "Parses through the entire data-set file grabbing all the variables.  The one issue is this is pretty inefficient in terms of memory, we load everything,
+then remove duplicates afterwards"
+  (with-data-file (s file)
+    (let* ((fline (read-csv-line s))
+	   (allvars (map 'list #'(lambda (x) (list x)) fline)))
+      (loop for line = (read-csv-line s) while line do
+	   (loop for x = 0 then (+ 1 x) for cols in line do
+		(setf (elt allvars x) (append (elt allvars x) (set-difference (list cols) (elt allvars x) :test #'string-equal)))) finally (return allvars) ))))
+
 (defun getClassVariables (&key (index 0) (file *data-set-file*))
   "Given an a default index 0 (first column), we parse the data set file to determine all the possible value options for our class."
   (with-data-file (s file)
