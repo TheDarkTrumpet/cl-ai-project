39ac0d7d467325e07ec5b35930dc4ceb5202db99
David Thole <dthole@x02c3.local>
2010-04-30 06:00:56 -0500

    Adding a count-all-attributes, this will be used in conjunction with class-probability to do the bayesian probability stuff

--- lib/learners/nb/nb.lisp
+++ lib/learners/nb/nb.lisp
@@ -5,9 +5,23 @@
 
 (in-package :ai-bayes)
 
-(defvar n-class nil)
+(defvar *n-class* nil)
 
-(defvar p-class nil)
+(defvar *p-class* nil)
 
-(defun count-all-attributes-in-classes ()
-  
+(defun count-all-attributes-in-classes (class-index data-set class-variables)
+  (let ((class-count (make-hash-table :size (length class-variables))))
+    (dolist (class class-variables)
+      (setf (gethash (intern class) class-count) 0))
+    (dolist (line data-set)
+      (incf (gethash (intern (elt line class-index)) class-count)))
+    (setf *n-class* class-count)
+    class-count))
+
+(defun class-probability ()
+  )
+
+(defun nb ()
+  "Our only exposed naive-bayes function that'll perform the training")
+
+
