ca061785e96ec34d94af2d90990b298b00156fe5
David Thole <dthole@x02c3.local>
2010-05-02 20:29:40 -0500

    Massive Comments...

--- lib/main.lisp
+++ lib/main.lisp
@@ -5,6 +5,15 @@
 (in-package :ai)
 
 (defun analyze-results (testing-results testing-set &key (class-index 0))
+  "This function takes a results list and a testing list as well as an optional class-index. We loop through each element of the lists, and we check to see if the elements are equal - if they are, then we increment the correct-classifications else we increment the incorrect-classifications.  Keep in mind the normal type of input we're getting is:
+testing-results: (...
+                 ((class1 . classp) ... (classN . classp))
+                 ...)
+ where class1 is a string, and classp is a floating point number less than 1 that represents the probability.  Since we're concerned with only the first element since it's ordered by the classification algorithm, we take just the first element.
+testing-set: (...
+              (attrib0 attrib1 attrib2 ... attribN)
+              ...)
+where attrib-X defined as the class-index is our class variable that's used in comparison"
   (let ((correct-classifications 0)
 	(incorrect-classifications 0))
     (loop for x in testing-results
@@ -20,6 +29,7 @@
 		      incorrect-classifications (/ incorrect-classifications (+ correct-classifications incorrect-classifications) 1.0))))))
 
 (defun display-results (results)
+  "This function takes a list of lists (2 elements in the inner list) which is the result of calling analyze-results.  We output out the presentation layer of what we came up with, including an average percentage and the details.  See run-nb's documentation for example output"
   (format t "Classification algorithm finished:
 Accuracy Listing: ~{~a, ~}
 Accuracy Average: ~a
@@ -31,6 +41,24 @@ Details returned:
 	  (mapcar #'second results)))
 
 (defun run-nb (folds)
+  "Pulls in the data set, class variables, splits it into 66% and 33% lengths.  Then, for each fold we create a
+random start varaible in the first 66% of the data, of that 33% of that goes into testing (using subseq, so
+this is not random as far as the data that goes into this list).  The rest of the data does into a training
+list.  From that, we bootstrap the training, class vars, and attribute vars.  We then call nb on the testing set,
+which will return a list of ordered class probabilities - which given that and the test set we pass to
+analyze results which will return two values - the first probability correct, and the second is a verbose message.
+We do this folds-number times pulling it into results.  At the end, we call display-results with this data.
+Output from this function looks something like the following, but only a small part of it:
+AI> (run-nb 1)
+Classification algorithm finished:
+Accuracy Listing: 0.99927616, 
+Accuracy Average: 0.99927616
+----------
+Details returned:
+Correct Classifications: 2761, 0.99927616
+Incorrect Classifications: 2, 7.238509e-4
+NIL
+"
   (let* ((data-set (getDataSet))
 	 (attributes (getAttributeVariables))
 	 (classvars (getClassVariables))
