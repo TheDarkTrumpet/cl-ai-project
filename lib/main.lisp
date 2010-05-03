6b7c7bbbc5c9e91a6233a59bd3ac7d8e59591627
David Thole <dthole@x02c3.local>
2010-05-02 19:52:02 -0500

    Modifications to run-nb, unstubbed analyze-results, but would like to include a bit more flexibility

--- lib/main.lisp
+++ lib/main.lisp
@@ -7,18 +7,34 @@
 
 "Classification algorithm:  Need algorithm name, chunk, eval-function"
 
-(defun analyze-results (testing-results testing-set)
+(defun analyze-results (testing-results testing-set &key (class-index 0))
+  (let ((correct-classifications 0)
+	(incorrect-classifications 0))
+    (loop for x in testing-results
+	 for y in testing-set
+	 do (if (equalp (car (first x))
+			(elt y class-index))
+		(incf correct-classifications)
+		(incf incorrect-classifications)))
+    (format t "Correct Classifications: ~a, ~a~%Incorrect Classifications: ~a, ~a~%"
+	    correct-classifications (/ correct-classifications (+ correct-classifications incorrect-classifications) 1.0)
+	    incorrect-classifications (/ incorrect-classifications (+ correct-classifications incorrect-classifications) 1.0))))
+
   (format t "~a : ~a~%" (first testing-results) (first testing-set)))
 
 (defun run-nb (folds)
-  (let ((data-set (getDataSet))
-	(attributes (getAttributeVariables))
-	(classvars (getClassVariables)))
+  (let* ((data-set (getDataSet))
+	 (attributes (getAttributeVariables))
+	 (classvars (getClassVariables))
+	 (66pc (floor (* (length data-set) .66)))
+	 (33pc (- (length data-set) 66pc))
+	 )
     (loop for x upto (- folds 1)
-       for y = (random (floor (* (length data-set) .66)))
-       for training-set = (subseq data-set y (+ y (- (length data-set) y)))
-       for testing-set = (remove-if #'identity data-set :start y :count (- (length data-set) y))
+       for random-start-var = (random 66pc)
+       for testing-set = (subseq data-set random-start-var (+ random-start-var 33pc))
+       for training-set = (remove-if #'identity data-set :start random-start-var :count 33pc)
 	 collect (progn
+		   (format t "r: ~a x: ~a : ~a ~a~%" random-start-var x (length training-set) (length testing-set))
 		   (ai-bayes::bootstrap :class-var-index 0 :training-set training-set :class-vars classvars :attribute-vars attributes)
 		   (analyze-results (ai-bayes::nb testing-set) testing-set)))))
 
